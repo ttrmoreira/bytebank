8dff458d06264938e135823f16a638d64629f3a8
ttrmoreira <ttrmoreira@gmail.com>
2020-11-05 10:19:09 -0300

    Projeto Byte Bank

---
+++ index.js
@@ -0,0 +1,31 @@
+
+class Cliente {
+    nome;
+    cpf;
+    agencia;
+    saldo;
+}
+
+const cliente1 = new Cliente();
+const cliente2 = new Cliente();
+const cliente3 = new Cliente();
+
+
+cliente1.nome = "Ricardo";
+cliente1.cpf = 11122233309;
+cliente1.agencia = 1001;
+cliente1.saldo = 0;
+
+cliente2.nome = "Alice";
+cliente2.cpf = 88822233309;
+cliente2.agencia = 1001; 
+cliente2.saldo = 0;
+
+cliente3.nome = "Alice";
+cliente3.cpf = 88822233309;
+cliente3.agencia = 1001;
+cliente3.saldo = 0;
+
+console.log(cliente1);
+console.log(cliente2);
+console.log(cliente3);
