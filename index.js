1f612ae5da032ff40ae1dc455eb6592d807c5001
ttrmoreira <ttrmoreira@gmail.com>
2020-11-09 09:16:53 -0300

    Elaboração de classes

--- index.js
+++ index.js
@@ -2,30 +2,46 @@
 class Cliente {
     nome;
     cpf;
-    agencia;
-    saldo;
 }
 
+class ContaCorrente {
+    agencia;
+    _saldo = 0;
+
+    sacar(valor){
+        if(this._saldo >= valor){
+            this._saldo -= valor;
+            return valor;
+        }
+    }
+
+    depositar(valor){
+        if(valor <= 0) {
+            return;
+        }
+        this._saldo += valor;
+    }
+}
+
+
 const cliente1 = new Cliente();
-const cliente2 = new Cliente();
-const cliente3 = new Cliente();
-
-
 cliente1.nome = "Ricardo";
 cliente1.cpf = 11122233309;
-cliente1.agencia = 1001;
-cliente1.saldo = 0;
 
+const cliente2 = new Cliente();
 cliente2.nome = "Alice";
 cliente2.cpf = 88822233309;
-cliente2.agencia = 1001; 
-cliente2.saldo = 0;
 
-cliente3.nome = "Alice";
-cliente3.cpf = 88822233309;
-cliente3.agencia = 1001;
-cliente3.saldo = 0;
+const contaCorrenteRicardo = new ContaCorrente();
+contaCorrenteRicardo.agencia = 1001;
+contaCorrenteRicardo.depositar(346);
 
-console.log(cliente1);
-console.log(cliente2);
-console.log(cliente3);
+const valorSacado =  contaCorrenteRicardo.sacar(50);
+console.log(valorSacado);
+
+
+
+
+
+console.log(contaCorrenteRicardo);
+
