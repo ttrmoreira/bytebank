d703341814c70fbe362dec94ef81b4c621cfa32f
ttrmoreira <ttrmoreira@gmail.com>
2020-12-29 11:50:47 -0300

    Herança, classes abstratas, e Duck Typing

--- SistemaAutenticacao.js
+++ SistemaAutenticacao.js
@@ -4,6 +4,14 @@
 
 export class SistemaAutenticacao{
     static login(autenticavel, senha){
-        return autenticavel.autenticar(senha);
+        if(SistemaAutenticacao.ehAutenticavel(autenticavel)){
+            return autenticavel.autenticar(senha);
+        }
+        return false;
+    }
+
+    static ehAutenticavel(autenticavel){
+        console.log(autenticavel);
+        return "autenticar" in autenticavel && autenticavel.autenticar instanceof Function;
     }
 }
